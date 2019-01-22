5741de5d2b325e9bbc93e0f6fbc7dae244d77d85
github username <github email address>
2019-01-22 12:15:42 -0500

    added write_review functoion for Restaurant, added base_logger ignore to environment

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -4,4 +4,10 @@ class Restaurant < ActiveRecord::Base
   has_many :reviews
   has_many :customers, through: :reviews
 
+  def write_review(customer, etiquette, punctuality, tipping)
+    #overall is int for now, change to float later
+    r = Review.create(customer: customer, restaurant: self, etiquette: etiquette, punctuality: punctuality, tipping: tipping, overall: ((etiquette + punctuality + tipping) / 3).round(1))
+    r.save
+  end
+
 end
--- config/environment.rb
+++ config/environment.rb
@@ -7,3 +7,5 @@ require_all 'app/'
 
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
 require_all 'lib'
+
+ActiveRecord::Base.logger = nil
