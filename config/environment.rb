a1c97fc715dee53dfc22973e11e3d76286c0189b
github username <github email address>
2019-01-22 12:01:46 -0500

    added required elements in environemnt

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,9 @@
 require 'bundler'
 Bundler.require
 
+require 'require_all'
+
+require_all 'app/'
+
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
 require_all 'lib'
