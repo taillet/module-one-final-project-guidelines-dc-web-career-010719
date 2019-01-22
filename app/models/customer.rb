5abeb46a72726b5e67c8e82f0c5b1a940d7455de
github username <github email address>
2019-01-22 15:13:35 -0500

    fixed missing parenthesis

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -13,15 +13,15 @@ class Customer < ActiveRecord::Base
   end
 
   def get_average_etiquette_score
-    self.get_all_customer_reviews.map{|i| i.etiquette}.inject{|sum, x| sum + x} / self.get_all_customer_reviews.size).round(1)
+    (self.get_all_customer_reviews.map{|i| i.etiquette}.inject{|sum, x| sum + x} / self.get_all_customer_reviews.size).round(1)
   end
 
   def get_average_punctuality_score
-    self.get_all_customer_reviews.map{|i| i.punctuality}.inject{|sum, x| sum + x} / self.get_all_customer_reviews.size).round(1)
+    (self.get_all_customer_reviews.map{|i| i.punctuality}.inject{|sum, x| sum + x} / self.get_all_customer_reviews.size).round(1)
   end
 
   def get_average_tipping_score
-    self.get_all_customer_reviews.map{|i| i.tipping}.inject{|sum, x| sum + x} / self.get_all_customer_reviews.size).round(1)
+    (self.get_all_customer_reviews.map{|i| i.tipping}.inject{|sum, x| sum + x} / self.get_all_customer_reviews.size).round(1)
   end
 
 end
