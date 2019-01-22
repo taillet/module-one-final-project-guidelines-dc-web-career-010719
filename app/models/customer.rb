bb17a2cbc4656b06a3a95fe9dcc85e9b17e5a600
github username <github email address>
2019-01-22 15:21:19 -0500

    added to_f to average methods

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -9,19 +9,19 @@ class Customer < ActiveRecord::Base
   end
 
   def get_average_rating
-    (self.get_all_customer_reviews.map{|i| i.overall}.inject{|sum, x| sum + x} / self.get_all_customer_reviews.size).round(1)
+    (self.get_all_customer_reviews.map{|i| i.overall}.inject{|sum, x| sum + x}.to_f / self.get_all_customer_reviews.size).round(1)
   end
 
   def get_average_etiquette_score
-    (self.get_all_customer_reviews.map{|i| i.etiquette}.inject{|sum, x| sum + x} / self.get_all_customer_reviews.size).round(1)
+    (self.get_all_customer_reviews.map{|i| i.etiquette}.inject{|sum, x| sum + x}.to_f / self.get_all_customer_reviews.size).round(1)
   end
 
   def get_average_punctuality_score
-    (self.get_all_customer_reviews.map{|i| i.punctuality}.inject{|sum, x| sum + x} / self.get_all_customer_reviews.size).round(1)
+    (self.get_all_customer_reviews.map{|i| i.punctuality}.inject{|sum, x| sum + x}.to_f / self.get_all_customer_reviews.size).round(1)
   end
 
   def get_average_tipping_score
-    (self.get_all_customer_reviews.map{|i| i.tipping}.inject{|sum, x| sum + x} / self.get_all_customer_reviews.size).round(1)
+    (self.get_all_customer_reviews.map{|i| i.tipping}.inject{|sum, x| sum + x}.to_f / self.get_all_customer_reviews.size).round(1)
   end
 
 end
